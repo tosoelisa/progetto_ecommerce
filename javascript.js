b9b6c7bda187e0d4f99133ecb6b2448893cef410
tosoelisa <107490230+tosoelisa@users.noreply.github.com>
2023-04-21 14:22:10 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+let popup = document.getElementById("popup");
+let accetta = document.getElementById("accetta");
+let rifiuta = document.getElementById("rifiuta");
+
+function goUp() {
+  popup.style = "transform: translateY(-125px);";
+  accetta.style = "transform: translateY(-175px);";
+  rifiuta.style = "transform: translateY(-175px);";
+}
+
+function disappear() {
+  popup.style = "display: none;";
+  accetta.style = "display: none;";
+  rifiuta.style = "display: none;";
+}
