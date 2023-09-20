47fa38d664beea30d0a078dd930b39d37fdf579f
tosoelisa <107490230+tosoelisa@users.noreply.github.com>
2023-09-20 12:17:39 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,23 +1,3 @@
-function scrollToTop() {
-  window.scrollTo(0, 0);
-}
-
-let popup = document.getElementById("popup");
-let accetta = document.getElementById("accetta");
-let rifiuta = document.getElementById("rifiuta");
-
-function goUp() {
-  popup.style = "transform: translateY(-125px);";
-  accetta.style = "transform: translateY(-175px);";
-  rifiuta.style = "transform: translateY(-175px);";
-}
-
-function disappear() {
-  popup.style = "display: none;";
-  accetta.style = "display: none;";
-  rifiuta.style = "display: none;";
-}
-
 $('.slide-nav').on('click', function(e) {
   e.preventDefault();
   // get current slide
@@ -39,3 +19,25 @@ $('.slide-nav').on('click', function(e) {
     }, 800);
   }
 });
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+let popup = document.getElementById("popup");
+let accetta = document.getElementById("accetta");
+let rifiuta = document.getElementById("rifiuta");
+
+function goUp() {
+  popup.style = "transform: translateY(-125px);";
+  accetta.style = "transform: translateY(-175px);";
+  rifiuta.style = "transform: translateY(-175px);";
+}
+
+function disappear() {
+  popup.style = "display: none;";
+  accetta.style = "display: none;";
+  rifiuta.style = "display: none;";
+}
+
+
